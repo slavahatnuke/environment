aef52eebb8ff0dcca81f3da590d132ab47e336ee
Ton Sharp <66ton99@gmail.com>
2013-02-27 22:33:26 +0200

    Added short cuts for profiles

---
+++ src/Hat/Environment/Tester/ProfileShortCut.php
@@ -0,0 +1,23 @@
+<?php
+namespace Hat\Environment\Tester;
+
+use Hat\Environment\Tester;
+use Hat\Environment\TesterOutput;
+use Hat\Environment\Request\CliRequest;
+
+class ProfileShortCut extends Tester
+{
+    protected $defaults = array(
+        'name' => null
+    );
+
+    public function test()
+    {
+
+        $request = new CliRequest();
+        if ($this->has('name') && $request->has($this->get('name'))) {
+            return true;
+        }
+        return false;
+    }
+}
