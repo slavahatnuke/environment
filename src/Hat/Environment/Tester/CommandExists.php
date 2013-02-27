5eb78844be53c34dd7d390b44803c7f53c2b0259
slava hatnuke <slava.hatnuke@gmail.com>
2013-02-27 16:10:13 +0200

    extract finder to options

--- src/Hat/Environment/Tester/CommandExists.php
+++ src/Hat/Environment/Tester/CommandExists.php
@@ -6,11 +6,15 @@ use Hat\Environment\TesterOutput;
 
 class CommandExists extends Tester
 {
+    protected $defaults = array(
+        'finder' => 'which'
+    );
+
     public function test()
     {
         //TODO [extract][cli][component] extract to CLI component
         $command = $this->get('command');
-        $cmd = 'which ' . $command;
+        $cmd = $this->get('finder') . ' ' . $command;
         $output = '';
         exec($cmd, $output, $return);
 
