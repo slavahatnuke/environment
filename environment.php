8a9598c8df2f352fc3e3d12d045eb86fb77fc5df
slava hatnuke <slava.hatnuke@gmail.com>
2013-02-28 12:39:31 +0200

    environment.ini for the root of project

--- environment.php
+++ environment.php
@@ -9,7 +9,7 @@ $env = new Environment();
 $env->handle(
     new CliRequest(
         array(
-            'profile' => 'profiles.ini'
+            'profile' => 'environment.ini'
         )
     )
 );
